0cef9d359e822e2b891d993a8c39c3565f7620e3
FacundoCien <130766359+FacundoCien@users.noreply.github.com>
2024-05-21 19:36:48 -0400

    titulo mardelplata

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,14 +13,19 @@ export default function App() {
     <>
       <Parallax>
         <header className="text-white text-center">
-          <div className="mx-auto max-w-xl pt-10">
-            <h1 className="font-bold text-3xl md:text-5xl lg:text-6xl mb-1 md:mb-2 lg:mb-3">
-              <span className="text-4xl md:text-6xl lg:text-7xl">E. E. S. T. N°5</span>
-              <hr className="my-1 md:my-2 lg:my-3 mx-auto w-1/2 md:w-2/3 lg:w-1/2 border-2" />
+          <div class="mx-auto max-w-xl pt-10">
+            <h1 class="font-bold text-3xl md:text-5xl lg:text-6xl ">
+              <span class="text-4xl md:text-6xl lg:text-7xl">
+                E. E. S. T. N°5
+              </span>
             </h1>
-            <h2 className="font-semibold text-2xl md:text-3xl lg:text-4xl mt-1 md:mt-2 lg:mt-3">
+            <h2 class="font-semibold text-xl md:text-2xl lg:text-3xl">
               Amancio Williams
             </h2>
+            <hr class="my-1 md:my-2 lg:my-3 mx-auto w-1/4 md:w-1/3 lg:w-1/3 border-1" />
+            <h3 class="text-1xl md:text-2xl lg:text-2xl">
+              Mar Del Plata
+            </h3>
           </div>
         </header>
         <body className=""></body>
@@ -28,7 +33,11 @@ export default function App() {
           <HomeButton text="Nuestro Contacto" img={comunicar} to="/contacto" />
           <HomeButton text="Ver Anuncios" img={anuncio} to="/anuncios" />
           <HomeButton text="Sobre Nosotros" img={equipo} to="/nosotros" />
-          <HomeButton text="Nuestras Especialidades" img={experto} to="/especialidades" />
+          <HomeButton
+            text="Nuestras Especialidades"
+            img={experto}
+            to="/especialidades"
+          />
         </main>
       </Parallax>
       <Footer />
