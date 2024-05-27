a4ee29cd3552c01e373f04c52602f8ddcdcd18d3
GudinoJoaquin <JoaGudi@hotmail.com>
2024-05-27 09:04:31 -0300

    gfdfg

--- src/components/AdminCard.jsx
+++ src/components/AdminCard.jsx
@@ -90,9 +90,9 @@ export default function AdminCard({
         <div className="p-6 xl:flex-1">
           <div className="flex">
             <div className="flex-1 mr-[20px]">
-              <div className="flex justify-around">
-                <h5 className="font-bold text-[30px] relative right-[5vw]">{titulo}</h5>
-                <h5 className="font-semibold text-[20px] ml-[20px] mt-[10px] text-blue-500">
+              <div className="flex justify-between">
+                <h5 className="font-bold text-[30px]">{titulo}</h5>
+                <h5 className="font-semibold text-[20px] ml-[20px] mt-[10px] text-blue-500 flex justify-end">
                   {fecha}
                 </h5>
               </div>
