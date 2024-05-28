b5db278460f2bcc77e11fbd483e70033cb6810d2
FacundoCien <130766359+FacundoCien@users.noreply.github.com>
2024-05-28 14:08:00 -0400

    aa

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,14 +4,22 @@ export default function TitleHome() {
   return (
     <header className="text-white text-center">
       <div className="mx-auto max-w-xl pt-10">
-        <h1 className={`font-bold text-3xl md:text-5xl lg:text-6xl animate-fade-in-down`}>
-          <span className="text-4xl md:text-6xl lg:text-7xl">E. E. S. T. N°5</span>
+        <h1
+          className={`font-bold text-3xl md:text-5xl lg:text-6xl animate-fade-in-down`}
+        >
+          <span className="text-4xl md:text-6xl lg:text-7xl">
+            E. E. S. T. N°5
+          </span>
         </h1>
-        <h2 className={`font-semibold text-xl md:text-2xl lg:text-3xl animate-pulse-fade-in`}>
+        <h2
+          className={`font-semibold text-xl md:text-2xl lg:text-3xl animate-pulse-fade-in`}
+        >
           Amancio Williams
         </h2>
-        <hr className={`my-1 md:my-2 lg:my-3 mx-auto w-1/4 md:w-1/3 lg:w-1/3 border-1  animate-expand-horizontally`} />
-        <h2 className={`text-1xl md:text-2xl lg:text-2xl  animate-zoom-in`}>
+        <hr
+          className={`my-1 md:my-2 lg:my-3 mx-auto w-1/4 md:w-1/3 lg:w-1/3 border-1  animate-expand-horizontally`}
+        />
+        <h2 className={`text-lg md:text-xl lg:text-xl animate-zoom-in`}>
           Mar Del Plata
         </h2>
       </div>
