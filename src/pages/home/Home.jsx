5918db9b7fb4bb94d9551e5e00b2a015c04676be
GudinoJoaquin <JoaGudi@hotmail.com>
2024-11-07 12:34:07 -0300

    prueba botones 3

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -49,12 +49,12 @@ export default function Home() {
 
   return (
     <div className="layout">
-      <div className="bg-black xl:mb-[10px]">
+      <div className="bg-black">
         <Parallax>
           <TitleHome />
           <main className="mt-[10vw] xl:mt-0 2xl:mt-0">
             <div className="flex justify-center mt-[30px]">
-              <div className="xl:w-[400px] xl:h-[150px] w-[300px] h-[80px] border border-white xl:rounded-[20px] rounded-[10px] flex justify-center items-center">
+              <div className="xl:w-[400px] xl:h-[150px] w-[300px] h-[70px] border border-white xl:rounded-[20px] rounded-[10px] flex justify-center items-center">
                 <p className="text-white text-center xl:text-[20px] text-[12px] font-semibold">
                   Se acerca la ExpoTec 2024. Los días miercoles 13 y jueves 14
                   de noviembre
