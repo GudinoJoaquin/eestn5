158f45887a671d6bd2cc77e15f50c2fbfed04037
GudinoJoaquin <JoaGudi@hotmail.com>
2024-11-11 18:48:01 -0300

    prueba galeria 5

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,7 +10,7 @@ export default function Modal({ img, alt, onClose }) {
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
       onClick={handleOverlayClick}
     >
-      <div className="bg-white w-[60vw] h-[35vw] max-w-[60vw] max-h-[60vw] flex backdrop-brightness-50">
+      <div className="bg-white w-[90vw] h-[70vw] flex backdrop-brightness-50">
         <div className="flex-1">
           <img
             src={img}
@@ -23,7 +23,7 @@ export default function Modal({ img, alt, onClose }) {
           <p className="mb-[20px] text-[1vw] border-b-[2px] border-gray-400/50 p-[5px]">
             Fecha: 01/01/01
           </p>
-          <p>
+          <p className="text-[8px]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
             iste ipsum, doloremque consequuntur blanditiis quod aliquam aut
             fugit at aliquid pariatur. Eum exercitationem doloribus, libero
