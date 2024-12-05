ff7484c4896b968d7280d0f67ebeb0ea3b2e6302
GudinoJoaquin <JoaGudi@hotmail.com>
2024-12-05 15:59:42 -0300

    regimen academico 2

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -5,7 +5,7 @@ export default function Noticia({ href, text, boldText }) {
         onClick={() => (window.location.href = { href })}
         className="xl:w-[400px] xl:h-[150px] w-[320px] h-[80px] border border-white xl:rounded-[20px] rounded-[10px] hover:scale-[1.10] flex justify-center items-center transition duration-[.3s] hover:cursor-pointer"
       >
-        <p className="text-white text-center xl:text-[20px] text-[12px] font-medium">
+        <p className="text-white text-center xl:text-[20px] text-[16px] font-medium">
           {text} <p className="underline font-bold">{boldText}</p>
         </p>
       </div>
