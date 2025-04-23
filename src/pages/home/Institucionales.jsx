c6edff16f7ed7eeedabd7e3b3d48240a8bc4e73c
GudinoJoaquin <JoaGudi@hotmail.com>
2025-04-23 14:33:41 -0300

    texto agregado

--- src/pages/home/Institucionales.jsx
+++ src/pages/home/Institucionales.jsx
@@ -11,17 +11,17 @@ export default function Institucionales() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const sec = queryParams.get("sec") || "ra";
+  const sec = queryParams.get("sec") || "ai";
   const [section, setSection] = useState(sec);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleClick = () => {
     contentRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
-    navigate(`/institucionales?sec=${section}`)
-  }, [section])
+    navigate(`/institucionales?sec=${section}`);
+  }, [section]);
 
   return (
     <body className="bg-white dark:bg-[#2D2D2D] dark:text-white">
@@ -108,46 +108,80 @@ export default function Institucionales() {
             <h2 className="mb-[20px] font-bold text-[30px]">
               Acuerdo institucional
             </h2>
-            <p className="mx-[60px] my-[10px] text-center lg:text-wrap lg:text-left">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-              rerum error similique? At neque fugiat excepturi iusto eum
-              molestiae eaque. Quasi sit, placeat consequatur quas nostrum
-              accusantium illum vitae at.
-            </p>
-            <p className="mx-[60px] my-[10px] text-center lg:text-wrap lg:text-left">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-              rerum error similique? At neque fugiat excepturi iusto eum
-              molestiae eaque. Quasi sit, placeat consequatur quas nostrum
-              accusantium illum vitae at.
-            </p>
-            <p className="mx-[60px] my-[10px] text-center lg:text-wrap lg:text-left">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-              rerum error similique? At neque fugiat excepturi iusto eum
-              molestiae eaque. Quasi sit, placeat consequatur quas nostrum
-              accusantium illum vitae at.
-            </p>
+            <section className="flex flex-col mx-[30px]">
+              <h2 className="font-semibold text-[25px]">Presentación</h2>
+              <div className="my-[10px] text-left indent-4  lg:text-wrap lg:text-left flex flex-col gap-[20px]">
+                <p>
+                  La educación técnico profesional introduce a los estudiantes,
+                  jóvenes y adultos, en un recorrido de profesionalización a
+                  partir del acceso a una base de conocimientos y de habilidades
+                  profesionales que les permita su inserción en áreas
+                  ocupacionales cuya complejidad exige haber adquirido una
+                  formación general, una cultura científica tecnológica
+                  profesional, así como continuar aprendiendo durante toda su
+                  vida.
+                </p>
+                <p>
+                  A las familias, docentes y estudiantes del Establecimiento
+                </p>
+                <p>
+                  La redacción de este AIC sostiene en primer lugar la finalidad
+                  educativa, como referente principal de las normas internas de
+                  esta institución. Es objetivo de la misma educar para el
+                  crecimiento personal y la participación responsable de los
+                  estudiantes, favoreciendo elejercicio de la libertad, el
+                  compromiso, el dialogo y la valoración de las diferencias. La
+                  Resolución 1709/09 de la Provincia de Buenos Aires proporciona
+                  un nuevo intento para la transformación educativa: valorar a
+                  la escuela como unidad que reflexiona sobre sus problemas y
+                  que tiene la facultad de diseñar estrategias de resolución.
+                  Mediante el trabajo en equipo que prioriza las cuestiones
+                  pedagógicas, la Institución genera mecanismos que mejores los
+                  aprendizajes de todos los estudiantes y un clima de
+                  convivencia democrática,{" "}
+                  <strong>
+                    {" "}
+                    promoviendo que todos son sujetos de respecto.
+                  </strong>
+                </p>
+                <p>
+                  El proceso de elaboración y puesta en marcha del AIC requiere
+                  de una cultura de colaboración entre quienes participan como
+                  integrantes de la comunidad educativa. En este sentido, el
+                  trabajo en equipo adquiere un valor fundamental como
+                  herramienta de intercambio entre personas: adolescentes,
+                  jóvenes y adultos que buscan arribar a acuerdos. Los
+                  principios fundacionales de este AIC son:{" "}
+                  <strong> Cooperativismo y solidaridad: </strong> es necesario
+                  aprender a trabajar juntos, sumando esfuerzos y compartir con
+                  los demás, ejerciendo la confianza y la ayuda mutua.
+                </p>
+                <p>
+                  <strong>Comunicación: </strong> los miembros de la comunidad,
+                  jóvenes y adultos, deben aprender a observar cuidadosamente, a
+                  comunicarse con precisión y a escuchar sensiblemente. Aprecio
+                  por la diversidad: aprender a respetar y apreciar las
+                  diferencias entre personas
+                </p>
+                <p>
+                  <strong>Expresión positiva de las emociones:</strong> aprender
+                  a expresar los sentimientos a partirdel autocontrol,
+                  encauzando la ira, el enfado y la frustración hacia las formas
+                  no agresivas ni destructivas
+                </p>
+                <p>
+                  <strong>Resolución de situaciones conflictivas: </strong>
+                  aprender a responder creativamente a lascuestiones planteadas
+                  en el contexto de una comunidad solidaria y de apoyo. Este
+                  aprendizaje cooperativo implica para los jóvenes no solo
+                  aprender contenidos, sinoaprender a relacionarse con el otro
+                  en un clima de respeto y participación
+                </p>
+              </div>
+            </section>
           </main>
         )}
       </main>
-
-      {/* <main className="mt-8 mx-[30px] xl:mx-[70px] md:mt-24">
-          <section className=" w-full mb-[20px] mt-[90px]">
-            <article className="mb-[50px] border-b-2 pb-[30px] border-gray-600/50">
-              <h2 className="font-medium mb-[5px] lg:text-[25px]">5/12/24</h2>
-              <h2 className="font-bold mb-[10px] text-[25px] lg:text-[40px]">
-                Nuevo regimen academico
-              </h2>
-              <p className="mb-[10px] lg:text-[24px]">
-                Dejamos a disposición la información sobre el nuevo régimen
-                académico y videos explicativos hechos por alumnos que ayudarán
-                a entender lo que entrará en vigencia a partir del próximo año.
-              </p>
-              <div className="grid place-items-center">
-                <video src={video} controls autoPlay></video>
-              </div>
-            </article>
-          </section>
-      </main> */}
       <Footer modal={(estado) => setIsModalOpen(estado)} />
       {isModalOpen && <DevsModal onClose={() => setIsModalOpen(false)} />}
     </body>
